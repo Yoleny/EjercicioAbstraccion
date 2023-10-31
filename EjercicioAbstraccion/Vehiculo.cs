de6e06cb6817ff76ec35e3a0fe789a49b720bf8b
Yoleny <yoleny.salme28@gmail.com>
2023-10-31 12:49:05 -0600

    Se agrego vehiculo como clase padre.

---
+++ EjercicioAbstraccion/Vehiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion
+{
+    internal  abstract class Vehiculo
+    {
+        public string? Marca;
+        public string? Modelo;
+        public bool matricula;
+        public bool año;
+
+        public abstract void
+            MetodoAstracto1();
+        public abstract void
+            MetodoAstracto2();
+        public abstract void
+            MetodoAbstratcto3();
+    }
+
+}
